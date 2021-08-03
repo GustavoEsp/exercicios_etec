7dab2d6b4adfc808c7f8317338a4bf43ad78f602
Gustavo ESP <55250697+GustavoEsp@users.noreply.github.com>
2021-08-02 21:11:27 -0300

    Adicionando exercicio de velocidade

---
+++ C#/Velocidade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace velocidade
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("***********************");
+			Console.WriteLine("Cálculo de velocidade *");
+			Console.WriteLine("***********************");
+			
+			float velocidade, distancia, tempo;
+			
+			Console.Write("Informe a distância percorrida em metros:");
+			distancia = float.Parse(Console.ReadLine());
+			
+			Console.Write("Informe o tempo levado em minutos:");
+			tempo = float.Parse(Console.ReadLine());
+			
+			velocidade = distancia / tempo;
+			
+			Console.WriteLine("Um objeto que percorreu uma distância de " + distancia + " metros em " 
+			                  + tempo + " minutos fez o trajeto em uma velocidade de " 
+			                  + velocidade + " metros por minuto");
+			
+			Console.Write("Pressione qualquer tecla para sair . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
